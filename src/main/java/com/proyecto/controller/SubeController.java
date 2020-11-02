bc280d12b9c4e27cff1ced80664b4cb40d9ee783
Melani Romero <m.romero99@hotmail.com>
2020-11-02 09:18:30 -0300

    SubeController con algunas modificaciones

--- src/main/java/com/proyecto/controller/SubeController.java
+++ src/main/java/com/proyecto/controller/SubeController.java
@@ -1,34 +1,35 @@
 package com.proyecto.controller;
+
 import com.proyecto.entity.Sube;
 import com.proyecto.service.SubeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
-import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.List;
-
 
 @RestController
 public class SubeController {
-    //controller advice
+
     @Autowired
-    private SubeService subeService ;
+    private SubeService subeService;
 
     @GetMapping("/subes")
-    List<Sube> getAll(){
+    List<Sube> getAll() {
         return subeService.getAllSube();
     }
 
     @GetMapping("/sube/{id}")
-    Sube get(@PathVariable Integer id ) throws Exception {
+    Sube get(@PathVariable Integer id) throws Exception {
         if (id == 10) {
-            throw new Exception();			}
+            throw new Exception();
+        }
         return subeService.getSubeById(id);
     }
-    @PutMapping("/sube/{id}/cargarSube")
-    Sube put(@PathVariable Integer id ,@RequestBody BigDecimal newBalance) throws Exception {
 
-        return subeService.putNewSaldo(id,newBalance);
+    @PutMapping("/sube/{id}/cargarSube")
+    Sube put(@PathVariable Integer id, @RequestBody BigDecimal newBalance) throws Exception {
+
+        return subeService.putNewSaldo(id, newBalance);
     }
 }
--- src/main/java/com/proyecto/service/TripService.java
+++ src/main/java/com/proyecto/service/TripService.java
@@ -1,8 +1,6 @@
 package com.proyecto.service;
 
-import com.proyecto.entity.Sube;
 import com.proyecto.entity.Trip;
-import com.proyecto.repository.SubeJPARepository;
 import com.proyecto.repository.TripJPARepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
